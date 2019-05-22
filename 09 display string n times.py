1fde08f86bdb531cbfcab9689ef2ddd8088cf8c2
Laiba Nasir <laibanasirr@gmail.com>
2019-05-22 18:30:09 +0500

    python task

---
+++ 09 display string n times.py
@@ -0,0 +1,5 @@
+#---- 09 Display string n times ----
+string = input('Enter message: ')
+num = int(input('How many times should it be displayed? '))
+for n in range (1, num + 1):
+    print(n , string)
