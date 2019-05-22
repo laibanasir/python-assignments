5decf74c829b2c2980cf243f7875773e0240cd44
Laiba Nasir <laibanasirr@gmail.com>
2019-05-22 18:27:01 +0500

    python task

--- 05 number of days bw two dates.py
+++ 05 number of days bw two dates.py
@@ -6,7 +6,7 @@ while True:
         date2 = input('Enter the second date : ')
         date_1 = date1.split('/')
         date_2 = date2.split('/')
-        diff = (int(date_2[2]) * 365 + int(date_2[1]) * 30 + int(date_2[0])) - (int(date_1[2]) * 365 + int(date_1[1]) * 30 + int(date_2[0]))
+        diff = (int(date_2[2]) * 365 + int(date_2[1]) * 30 + int(date_2[0])) - (int(date_1[2]) * 365 + int(date_1[1]) * 30 + int(date_1[0]))
         print('The number of days between' , date2 , 'and' , date1 , 'is' , diff ,'days.')
         break
     except:
